4590b953c3e1bdaacf909bc89bf6cf254ceb998e
Brijesh Brahmbhatt <69754342+Brijesh-3233@users.noreply.github.com>
2023-07-23 19:58:30 +0530

    Add files via upload

---
+++ React Components.jsx
@@ -0,0 +1,16 @@
+import Head from "./Head";
+import Para from "./Para";
+import List from "./List";
+
+function App()
+{
+    return(
+    <>
+    <Head/>
+    <Para/>
+    <List/>
+    </>
+    )
+}
+
+export default App;
